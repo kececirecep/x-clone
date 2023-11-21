67dbdbbdc67b583a5690508c3cd1fb5f7c126bfb
kececirecep <kececirecep@outlook.com>
2023-11-21 19:53:25 +0300

    trendlist-change

--- src/components/TrendList.js
+++ src/components/TrendList.js
@@ -14,18 +14,10 @@ const TrendList = () => {
           <h2 className='font-bold text-xl px-4 py-4'>İlginizi çekebilecek gündemler</h2>
           <ul className='mt-4 pb-4'>
           <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Alex de Souza <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Cristiano Ronaldo <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
+          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Lionel Messi <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
           <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Doktor <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
           <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Kolombiya <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
           <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#İsmail Kartal <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
-          <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
           <li className='font-bold text-md py-4 hover:bg-gray-200 px-4 cursor-pointer flex justify-between items-center'>#Arda Güler <span className='hover:bg-blue-200 hover:text-blue-600 p-2 rounded-full'><BsThreeDots/></span></li>
           </ul>
         </nav>
